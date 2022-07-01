961d008cb2cac49ce3635b43d519ea713ab7bedd
Akhdan24 <1998akhdan@gmail.com>
2022-07-01 17:45:08 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/timun/popup_update/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.timun.popup_update
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/popup.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class popup extends StatefulWidget {
+  const popup({Key? key}) : super(key: key);
+
+  @override
+  State<popup> createState() => _popupState();
+}
+
+class _popupState extends State<popup> {
+  var isChanged = false;
+  final Uri linkandroid = Uri.parse("https://play.google.com/store/apps/details?id=com.timun.timun_app");
+  final Uri linkios = Uri.parse("https://play.google.com/store/apps/details?id=com.timun.timun_app");
+
+  Future<void> show(BuildContext context) {
+    return showDialog(
+        context: context,
+        useSafeArea: false,
+        builder: (context) {
+          return AnimatedContainer(
+            duration: Duration(seconds: 1),
+            width: isChanged ? 275 : 0,
+            height: isChanged ? 350 : 0,
+            child: Container(
+              height: 350,
+              width: 275,
+              alignment: Alignment.center,
+              child: Container(
+                height: 375,
+                width: 275,
+                child: Stack(
+                  fit: StackFit.loose,
+                  alignment: Alignment.center,
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        height: 300,
+                        width: 275,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text("Update Aplikasi Kamu!",
+                                style: GoogleFonts.poppins().copyWith(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black)),
+                            SizedBox(height: 10),
+                            Container(
+                              child: Text(
+                                  "Perbarui aplikasi kamu dan dapatkan promo terbaru serta kenyamanan belanja",
+                                  maxLines: 2,
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.poppins().copyWith(
+                                      fontSize: 11,
+                                      fontWeight: FontWeight.w400,
+                                      color: "989797".toColor())),
+                            ),
+                            SizedBox(height: 20),
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 35,
+                              child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(4)),
+                                color: "AAD7DE".toColor(),
+                                onPressed: () async {
+                                  if (Platform.isAndroid) {
+                                    if (!await launchUrl(linkandroid)) throw 'Could not launch $linkandroid';
+                                  } else if (Platform.isIOS) {
+                                    if (!await launchUrl(linkios)) throw 'Could not launch $linkios';
+                                  }
+                                },
+                                child: Text("Update Sekarang",
+                                    style: GoogleFonts.poppins().copyWith(
+                                        fontSize: 11,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 35,
+                              child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4),
+                                  side: BorderSide(color: "AAD7DE".toColor(), width: 2),
+                                ),
+                                color: Colors.white,
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Nanti Dulu Aja!",
+                                    style: GoogleFonts.poppins().copyWith(
+                                        fontSize: 11,
+                                        fontWeight: FontWeight.bold,
+                                        color: "AAD7DE".toColor())),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      child: Container(
+                          width: 190,
+                          height: 190,
+                          padding: EdgeInsets.all(3),
+                          child: Image(
+                            image: AssetImage("assets/img_intro.png"),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() => isChanged = !isChanged);
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      show(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
